fddf5163f826f8abdfa6d3ba702ceba5a2d41e23
Edgar Lopez Calomarde <edgarlopezcalomarde@gmail.com>
2025-03-27 23:57:58 +0100

    chore(frontend): setup react app with vite

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import mkcert from "vite-plugin-mkcert";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss(), mkcert()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
